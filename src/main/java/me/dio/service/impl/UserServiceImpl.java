604398fffbc512562bcee0b993f1d2ddc9f8811b
rinaldo-JR1 <rinaldo3uchoa@gmail.com>
2024-07-23 17:22:30 -0300

    User service

---
+++ src/main/java/me/dio/service/impl/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package me.dio.service.impl;
+
+import me.dio.domain.model.User;
+import me.dio.domain.repository.UserRepository;
+import me.dio.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(null);
+    }
+
+    @Override
+    public User create(User user) {
+        return null;
+    }
+}
